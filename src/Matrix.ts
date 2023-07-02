6d3e314db819b21c28ae1986c3c1fcdc3729852b
Crieloue Gilles <gilles.crieloue@gmail.com>
2023-07-02 21:53:45 +0200

    'dev'

--- src/Matrix.ts
+++ src/Matrix.ts
@@ -16,7 +16,7 @@ export const matrix = new LedMatrix(
       type: PixelMapperType.U,
     }),
     pwmBits: 11, //default 11
-    pwmLsbNanoseconds: 220, //default 130
+    pwmLsbNanoseconds: 250, //default 130
   },
   {
     ...LedMatrix.defaultRuntimeOptions(),
