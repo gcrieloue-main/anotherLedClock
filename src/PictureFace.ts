47c0e78bd6c6fc3610d29b35ba4952bf51510e25
Crieloue Gilles <gilles.crieloue@gmail.com>
2023-07-04 22:40:23 +0200

    'dev'

---
+++ src/PictureFace.ts
@@ -0,0 +1,34 @@
+import { LedMatrixInstance } from "rpi-led-matrix";
+import { MatrixConfig } from "./MatrixConfig";
+import { wait } from "./Utils";
+var getPixels = require("get-pixels");
+
+export class ImageFace implements Face {
+  public enabled = true;
+  matrix: LedMatrixInstance;
+  offset: number = 0;
+  config: MatrixConfig;
+
+  constructor(ledMatrix: LedMatrixInstance, config: MatrixConfig) {
+    this.matrix = ledMatrix;
+    this.config = config;
+  }
+
+  public async display() {
+    this.matrix.clear();
+
+    getPixels("icon.png", function (err, pixels) {
+      if (err) {
+        console.log("Bad image path");
+        return;
+      }
+      console.log("got pixels", pixels.shape.slice());
+    });
+
+    this.matrix.fgColor(parseInt("0xff0000")).setPixel(0, 0);
+
+    await wait(10000);
+
+    this.matrix.sync();
+  }
+}
