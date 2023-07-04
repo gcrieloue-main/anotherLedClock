0bcfbd7bf3aa6dff0dfee97e957af9191883e274
Crieloue Gilles <gilles.crieloue@gmail.com>
2023-07-04 22:56:26 +0200

    'dev'

--- src/PictureFace.ts
+++ src/PictureFace.ts
@@ -19,11 +19,28 @@ export class PictureFace implements Face {
 
     getPixels(
       "./src/icon.png",
-      function (err: any, pixels: { shape: string | any[] }) {
+      function (
+        err: any,
+        pixels: {
+          shape: string | any[];
+          get: (arg0: number, arg1: number, arg2: number) => string;
+        }
+      ) {
         if (err) {
           console.log("Bad image path");
           return;
         }
+
+        for (let y = 0; y < pixels.shape[1]; y++) {
+          for (let x = 0; x < pixels.shape[0]; x++) {
+            const r = pixels.get(x, y, 0);
+            const g = pixels.get(x, y, 1);
+            const b = pixels.get(x, y, 2);
+            const rgba = `color: rgba(${r}, ${g}, ${b});`;
+            console.log(rgba);
+          }
+        }
+
         console.log("got pixels", pixels.shape.slice());
       }
     );
