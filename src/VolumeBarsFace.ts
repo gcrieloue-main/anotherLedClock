c9f80dc6214fa3afa0774d2fb0cfc22195ef066e
Crieloue Gilles <gilles.crieloue@gmail.com>
2023-07-08 15:21:07 +0200

    'dev'

--- src/VolumeBarsFace.ts
+++ src/VolumeBarsFace.ts
@@ -41,8 +41,18 @@ export class VolumeBarsFace implements Face {
     this.volumeBars.push({ x: 10, level: randomIntFromInterval(0, 15) });
     this.volumeBars.push({ x: 12, level: randomIntFromInterval(0, 15) });
     this.volumeBars.push({ x: 14, level: randomIntFromInterval(0, 15) });
+    this.volumeBars.push({ x: 16, level: randomIntFromInterval(0, 15) });
+    this.volumeBars.push({ x: 18, level: randomIntFromInterval(0, 15) });
+    this.volumeBars.push({ x: 20, level: randomIntFromInterval(0, 15) });
+    this.volumeBars.push({ x: 22, level: randomIntFromInterval(0, 15) });
+    this.volumeBars.push({ x: 24, level: randomIntFromInterval(0, 15) });
+    this.volumeBars.push({ x: 26, level: randomIntFromInterval(0, 15) });
+    this.volumeBars.push({ x: 28, level: randomIntFromInterval(0, 15) });
+    this.volumeBars.push({ x: 30, level: randomIntFromInterval(0, 15) });
 
     this.matrix.afterSync(() => {
+      this.matrix.clear();
+
       const color: number = parseInt("0x" + randomElement(Colors));
       this.volumeBars.forEach((bar) => {
         if (randomBoolean() && bar.level < 15) {
