b3cef59bb0c8d8413a4b25559c3490e59e0a38a0
Crieloue Gilles <gilles.crieloue@gmail.com>
2023-07-10 21:59:57 +0200

    'dev'

--- src/ClockFace.ts
+++ src/ClockFace.ts
@@ -7,6 +7,7 @@ const fontTom = new Font('tom', 'fonts/tom-thumb.bdf')
 export enum ClockStyleEnum {
   BORDER = 'BORDER',
   CLASSIC = 'CLASSIC',
+  TWENTY_FOUR = 'TWENTY_FOUR',
 }
 
 export class ClockFace implements Face {
@@ -38,24 +39,51 @@ export class ClockFace implements Face {
     this.matrix.sync()
   }
 
-  private async displayClock() {
+  private displayAmpPmClock() {
     const time = new Date()
     const timeStr = this.formatTime(time)
     const ampmStr = this.formatAMPM(time)
     //console.log("display", timeStr);
     this.matrix
       .clear()
-      .fgColor(0x000000)
-      .fill()
       .fgColor(this.config.primaryColor)
       .font(fontTom)
       .drawText(timeStr, (time.getHours() + 1) % 12 > 9 ? 2 : 3, 5)
+    this.matrix
+      .fgColor(this.config.secondaryColor)
+      .font(font4x6)
+      .drawText(ampmStr, 23, 5)
+  }
 
+  private display24Clock() {
+    const date = new Date()
+    var hours = date.getHours() + 1
+    var minutes = date.getMinutes()
+    var strHours = hours < 10 ? '0' + hours : hours
+    var strMinutes = minutes < 10 ? '0' + minutes : minutes
+    var timeStr = strHours + (this.dotDisplayed ? ':' : ' ') + strMinutes
+
+    this.matrix
+      .clear()
+      .fgColor(this.config.primaryColor)
+      .font(fontTom)
+      .drawText(
+        timeStr,
+        (this.matrix.width() - font4x6.stringWidth(timeStr)) / 2,
+        5,
+      )
+  }
+
+  private async displayClock() {
     if (this.style == ClockStyleEnum.CLASSIC) {
+      this.displayAmpPmClock()
+
       this.matrix
         .fgColor(this.config.alternateColor)
         .drawRect(0, this.matrix.height() - 2, this.matrix.width() - 1, 1)
     } else if (this.style == ClockStyleEnum.BORDER) {
+      this.displayAmpPmClock()
+
       this.matrix
         .fgColor(this.config.alternateColor)
         .drawRect(0, 0, this.matrix.width() - 1, this.matrix.height() - 1)
@@ -64,12 +92,9 @@ export class ClockFace implements Face {
         .setPixel(0, this.matrix.height() - 1)
         .setPixel(this.matrix.width() - 1, 0)
         .setPixel(this.matrix.width() - 1, this.matrix.height() - 1)
+    } else if (this.style == ClockStyleEnum.TWENTY_FOUR) {
+      this.display24Clock()
     }
-
-    this.matrix
-      .fgColor(this.config.secondaryColor)
-      .font(font4x6)
-      .drawText(ampmStr, 23, 5)
 
     this.dotDisplayed = !this.dotDisplayed
   }
